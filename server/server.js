0c42aae34e19eb424bb67bfc7671cf17766af48e
Ayush goyal <149871874+AyushPq777@users.noreply.github.com>
2025-11-21 06:20:34 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: ["http://localhost:3000", "http://127.0.0.1:3000", "https://codecollab-mtyy42pot-aayush-goyals-projects.vercel.app"],
+        origin: ["http://localhost:3000", "http://127.0.0.1:3000", "https://codecollab-client-x98j.onrender.com" ],
         methods: ["GET", "POST", "PUT", "DELETE"],
         credentials: true
     }
